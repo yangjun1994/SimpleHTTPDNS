f8ba2b339a46e2957ea7a34fa539d632a7c977f7
lagrenge <lag@lagtea.com>
2016-05-06 17:27:18 +0800

    添加Bootstrap框架

--- Server/admintools/changedomaindb.php
+++ Server/admintools/changedomaindb.php
@@ -11,8 +11,31 @@ if (isset($_POST['domain'])) {
     adddata($_POST['domain'], $_POST['ip'], $_POST['priority'], $_POST['src']);
 }
 echo <<<EOF
+<!DOCTYPE html>
+
+<html lang="zh-CN">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="Lagrenge的导航页">
+    <meta name="author" content="Lagrenge">
+    <meta name="theme-color" content="#0099cc">
+
+    <title>Domain DB</title>
+
+    <link href="https://www.lagtea.com/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://www.lagtea.com/css/lagtea.css" rel="stylesheet">
+    <link href="./i.css" rel="stylesheet">
+    <script src="https://www.lagtea.com/js/ie-emulation-modes-warning.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
 <h1>Domain DB</h1>
-<table border="1">
+<table class="table table-striped">
 <tr>
 <th>ID</th>
 <th>Domain</th>
@@ -40,15 +63,36 @@ while($resultline = mysqli_fetch_array($result)) {
 }
 echo '</table>';
 echo '
-<form action="'.$_SERVER['PHP_SELF'].'" method="post">
-Add Domain: <input type="text" name="domain">
-IP: <input type="text" name="ip">
-Priority: <input type="text" name="priority">
-Src: <input type="text" name="src">
-<input type="submit">
+<form class="form-group" action="'.$_SERVER['PHP_SELF'].'" method="post">
+Add Domain: <input class="form-control" type="text" name="domain">
+IP: <input class="form-control" type="text" name="ip">
+Priority: <input class="form-control" type="text" name="priority">
+Src: <input class="form-control" type="text" name="src">
+<input class="btn btn-default" type="submit">
 </form>
 ';
-echo "<a href=".$_SERVER['PHP_SELF']."?clear=1>Clear <b>ALL</b> data</a>";
+echo "<a class='btn btn-danger' href=".$_SERVER['PHP_SELF']."?clear=1>Clear <b>ALL</b> data</a>";
+
+
+echo <<<EOF
+
+        <footer>
+            <p>© Lagrenge 2016-2017</p>
+        </footer>
+    </div>
+    <script src="https://www.lagtea.com/js/jquery.min.js"></script>
+    <script src="https://www.lagtea.com/js/bootstrap.min.js"></script>
+    <script src="https://www.lagtea.com/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
+EOF;
+
+
+
+
+
+
+
 
 function deldata($id) {
     include '../conn.php';
--- Server/admintools/showcache.php
+++ Server/admintools/showcache.php
@@ -5,8 +5,31 @@ if (isset($_GET['clear'])) {
     clearcache();
 }
 echo <<<EOF
+<!DOCTYPE html>
+
+<html lang="zh-CN">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="Cache">
+    <meta name="author" content="Lagrenge">
+    <meta name="theme-color" content="#0099cc">
+
+    <title>Domain DB</title>
+
+    <link href="https://www.lagtea.com/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://www.lagtea.com/css/lagtea.css" rel="stylesheet">
+    <link href="./i.css" rel="stylesheet">
+    <script src="https://www.lagtea.com/js/ie-emulation-modes-warning.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
 <h1>Cache</h1>
-<table border="1">
+<table class="table table-striped" >
 <tr>
 <th>Domain</th>
 <th>IP</th>
@@ -25,5 +48,17 @@ while($resultline = mysqli_fetch_array($result)) {
 
 }
 echo '</table>';
-echo "<a href=".$_SERVER['PHP_SELF']."?clear=1>Clear Cache</a>";
+echo "<a class='btn btn-danger' href=".$_SERVER['PHP_SELF']."?clear=1>Clear Cache</a>";
+echo <<<EOF
+
+        <footer>
+            <p>© Lagrenge 2016-2017</p>
+        </footer>
+    </div>
+    <script src="https://www.lagtea.com/js/jquery.min.js"></script>
+    <script src="https://www.lagtea.com/js/bootstrap.min.js"></script>
+    <script src="https://www.lagtea.com/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
+EOF;
 ?>
